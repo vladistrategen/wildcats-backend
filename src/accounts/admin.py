4927ac802d5263e2d10cd1b7c374a51d5bcce77c
Ianc Maria-Georgiana <iancmaria14@gmail.com>
2023-12-04 15:46:10 +0200

    created user groups

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from django.contrib.auth.models import Group
+
+# Register your models here, including the User model
+
+# Register the Group model to manage user groups in the admin interface
+#admin.site.unregister(Group)
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.contrib.auth.models import User, Group
+
+def create_user(request):
+    # Your user creation logic here
+
+    user = User.objects.create(username='example_user', password='example_password')
+
+    # Assign the user to a group based on their role
+    #user.groups.add(free_user_group)  # Replace with the appropriate group for the user
+
+    # Rest of your user creation logic
